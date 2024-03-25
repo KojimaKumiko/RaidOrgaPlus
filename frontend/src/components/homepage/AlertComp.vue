de97d4c1a3c6b0930890474eb63c863b3094762c
Kumiko <kumikozelenka@gmail.com>
2024-03-25 21:48:47 +0100

    fixed spelling of the texts

--- frontend/src/components/homepage/AlertComp.vue
+++ frontend/src/components/homepage/AlertComp.vue
@@ -3,28 +3,27 @@
 		<v-alert type="error" text v-if="noDiscordFound">
 			<h3>Kein Discord-Account gefunden</h3>
 			<p>
-				Für diesen RO+-Account konnte kein Discord-Account gefunden werden. Mögliche gründe dafür sind:
+				Für diesen RO+-Account konnte kein Discord-Account gefunden werden. Mögliche Gründe könnten sein:
 				<ul>
-					<li>Die Discord-API hate gerade probleme und es können keine Nutzer-Informationen darüber abgerfagt werden.</li>
-					<li>Auf dem Rising-Light Discord wurde kein Nutzer gefunden, dessen Nicknamen den Account-Namen <b>{{ user.accname }}</b> beinhaltet. Stelle hierfür bitte sicher, dass dein Nickname auf dem Discord auch deinen Account-Namen beinhält.</li>
+					<li>Auf dem Rising-Light Discord wurde kein Nutzer gefunden, dessen Nicknamen den Account-Namen <b>{{ user.accname }}</b> beinhaltet. Stelle bitte sicher, dass dein Nickname auf dem Discord auch deinen Account-Namen enthält.</li>
+					<li>Es können aufgrund von Problemen der Discord-API gerade keine Nutzer-Informationen abgefragt werden.</li>
 				</ul>
 			</p>
 		</v-alert>
 		<v-alert type="info" text v-else-if="applicationInProgress">
 			<h3>Bewerbung wird bearbeitet</h3>
 			<p>
-				Deine Bewerbung ist angekommen und wird bearbeitet. Dies kann etwa 1-2 Tage dauern. Stelle bitte in der
-				zwischenzeit sicher, dass dein Nickname auf dem Discord-Server und in RO+ übereinstimmen. <br />
-				Bei Fragen oder Problemen, kannst du dich jederzeit an jemanden aus dem Leitungsteam wenden.
+				Deine Bewerbung ist angekommen und wird bearbeitet. Dies kann einige Tage dauern.<br />
+				Bei Fragen oder Problemen kannst du dich jederzeit an jemanden aus dem Leitungsteam wenden.
 			</p>
 		</v-alert>
 		<v-alert type="info" text v-else-if="noApplication">
 			<h3>Keine Bewerbung</h3>
 			<p>
-				Es ist noch keine Bewerbung angekommen. Um dich zu bewerben, trete dafür dem <a href="https://join.rising-light.de">Rising-Light Discord</a> bei.
-				Anschließend findest du im Channel #erste-schritte eine Nachricht mit einem Bewerbungs-Knopf darunter. Klick dann auf den Bewerbungs-Knopf, füll das Formular aus welches erscheint und schick dieses ab.
-				Darauf folgend dürftest du eine Nachricht von unserem Bewerbungs-Bot erhalten haben. Wenn das der fall ist, ist deine Bewerbung erfolgreich bei uns angekommen und wird zeitig bearbeitet.<br />
-				Falls es zu problemen kommt oder du Fragen haben solltest, kannst du dich jederzeit an jemanden aus dem Leitungsteam wenden.
+				Für diesen Account liegt uns keine Bewerbung vor. Um dich zu bewerben, tritt dem <a href="https://join.rising-light.de">Rising-Light Discord</a> bei.
+				Dort findest du im Channel #erste-schritte eine Nachricht mit einem Bewerbungs-Knopf. Klicke auf den Bewerbungs-Knopf, fülle das Formular aus und schicke dieses ab.
+				Daraufhin solltest du eine Nachricht von unserem Bewerbungs-Bot erhalten. Wenn dies der Fall ist, ist deine Bewerbung erfolgreich bei uns angekommen und wird zeitnah bearbeitet.<br />
+				Falls es zu Problemen kommt oder du Fragen haben solltest, kannst du dich jederzeit an jemanden aus dem Leitungsteam wenden.
 			</p>
 		</v-alert>
 	</div>

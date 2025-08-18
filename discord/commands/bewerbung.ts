52ce3c4483008a115da6e4ad92acc30665d51b46
Kumiko <kumikozelenka@gmail.com>
2025-08-18 21:32:14 +0200

    updated welcome message

--- discord/commands/bewerbung.ts
+++ discord/commands/bewerbung.ts
@@ -9,6 +9,8 @@ import {
 	PermissionsBitField,
 	GuildMember,
 	Message,
+	userMention,
+	channelMention,
 } from "discord.js";
 import { defaultEmbed } from "../Utils/embedProvider";
 import { checkAccountName } from "../Utils/misc";
@@ -103,7 +105,9 @@ const executeCommand = async (interaction: MessageContextMenuCommandInteraction<
 
 			const welcomeChannel = await interaction.guild.channels.fetch(process.env.WELCOME_CHANNEL);
 			if (welcomeChannel && welcomeChannel.isTextBased()) {
-				const msg = `@silent ${guildMember.user.username} ist nun ein teil der Community!`;
+				const msg = `Hey ${userMention(guildMember.user.id)}, schön, dass du deinen Weg zu uns gefunden hast. <:aurene2:546815178440704001>
+Alle wichtigen Infos zur Community findest du in ${channelMention("504274282550132768")}. Falls du eine Raid-Gruppe suchst, schau doch mal in unsere ${channelMention("1047119423145791488")}, gib ein Gesuch unter ${channelMention("807294070173990952")} auf oder melde dich spontan auf ein Gesuch im ${channelMention("380353536745275393")}.
+Du bist die ersten 60 Tage als Newbie gekennzeichnet, dies hat aber keinen Einfluss auf deine Rechte in der Community.`;
 				await welcomeChannel.send(msg);
 			}
 		}
